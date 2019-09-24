f3a8177357d77f00ebe0205f495412670d173fe5
Daniel Paramo <danparamo@gmail.com>
2019-09-24 16:19:55 +0000

    initial commit

---
+++ Imageblur1.rb
@@ -0,0 +1,20 @@
+class Image
+    def initialize(pic)
+        @pic = pic
+    end
+
+    def output_image
+        @pic.each do |row|
+           puts row.join 
+    end
+end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
